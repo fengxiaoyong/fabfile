21a993d87e4cc20fe5539b52467507d382f0ddec
fengxiaoyong <fengxy@itv.cn>
2016-12-07 08:53:39 -0500

    add nginx git script

---
+++ fabfile_git.py
@@ -0,0 +1,48 @@
+from fabric.api import local,lcd,settings,run,abort,cd,env,put
+from fabric.contrib.console import confirm
+
+env.hosts=['192.168.8.87']
+#env.user='root'
+#env.password='yanlihua1982'
+env.key_filename='../.ssh/id_rsa'
+
+def shangchuan():
+	put('../git-2.10.2.tar.gz','/root')	
+
+def jieya():
+	with cd('/root'):
+		run('tar -zvxf git-2.10.2.tar.gz')	
+
+def install_before():
+	with settings(warn_only=True):
+		result=run('yum install -y gcc gcc+c++')
+		result1=run('yum install -y curl-devel expat-devel gettext-devel openssl-devel zlib-devel gcc perl-ExtUtils-MakeMaker')
+	if result.failed or result1.failed:
+		if not confirm('yum install failed.continue anyway?'):
+			abort('Aborting at user request')
+def install():
+        with cd('git-2.10.2'):
+		run('pwd')
+		with settings(warn_only=True):
+			result=run('make prefix=/usr/local/git all',)
+			if result.failed and not confirm('install failed.continue anyway?'):
+				abort('Aborting at user request')
+			result1=run('make prefix=/usr/local/git install')
+			if result1.failed and not confirm('install sencond failed continue anyway?'):
+				abort('Aborting at user request')
+def version():	
+	result=local('git --version')
+	pass
+
+def addenv():
+	run('echo "export PATH=/usr/local/git/bin:$PATH" >>/etc/profile')
+        run('source /etc/profile')
+
+def deploy():
+	shangchuan()
+	jieya()
+        install_before()
+	install()
+	addenv()
+	
+	
---
+++ fabfile_python3.py
@@ -0,0 +1,45 @@
+from fabric.api import *
+
+
+env.hosts=['192.168.8.87','192.168.9.79']
+#env.exclude_hosts=['192.168.9.79']
+filenames=['Python-3.5.2.tgz']
+UP_DIR='/tmp'
+env.key_filename='~/.ssh/id_rsa'
+def up_load():
+	#put('.ssh/id_rsa.pub' ,'/root')
+	for filename in filenames:
+		put(filename,UP_DIR)
+
+def before_intall():
+	with settings(warn_only=True):
+		run('yum -y install zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel readline-devel tk-devel gcc make')
+def tar():
+	with cd(UP_DIR):
+		for filename in filenames:
+			run('tar -zvxf %s' %filename)
+			run('rm -f %s '%filename)
+def install():
+	with cd(UP_DIR):
+		for filename in filenames:
+			end_index=filename.index('.tgz')
+			dir_1=filename[:end_index]
+			with cd(dir_1):
+				run('./configure --prefix=/usr/local/python35')
+				run('make')
+				run('make install')
+				with cd('..'):
+					run('rm -rf %s' %dir_1)
+@task
+def env_create():
+	run('yes|cp /etc/profile /etc/profile.bak')
+	run('echo "export PATH=/usr/local/python35/bin:$PATH" >>/etc/profile')
+	run('source /etc/profile')
+@task
+@parallel
+def deploy():
+	before_intall()
+	up_load()
+	tar()
+	install()
+	env_create()
